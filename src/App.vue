80a849ece4b9e91876d45a8e1470d40669c13ae7
eronzin13 <erontiago6@gmail.com>
2022-05-30 15:17:43 -0300

    terminado

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup></script>
+
+<template>
+  <header>home | Jogadores | Time</header>
+  <main>Main</main>
+  <footer>Rodapé</footer>
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  /* padding: 2rem; */
+  height: 100vh;
+
+  font-weight: normal;
+}
+header,
+footer {
+  background-color: pink;
+  color: red;
+  font-size: 1.3rem;
+  height: 15%;
+  display: flex;
+  align-items: center;
+  padding-left: 2rem;
+}
+
+main {
+  height: 70%;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>gabrielviado</h1>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+</style>
